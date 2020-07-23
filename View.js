609038f4995a26a6803d334f39640be70ba8db19
ysngr <junord182109@gmail.com>
2020-07-24 07:42:43 +0900

    Update View

--- View.js
+++ View.js
@@ -74,58 +74,30 @@ View.prototype.figure = function(r, c) {
 };
 
 
-View.prototype.getPanelColor = function(value) {
+View.prototype.getPanelColor = function(panelNum) {
 
-  let pnlCol;
-  switch(value) {
-  case 0 :   // empty panel color
-    pnlCol = color("#D3D3D3");  // frosty white
-    break;
-  case 2 : 
-    pnlCol = color("#A0D8EF");  // sora
-    break;
-  case 4 : 
-    pnlCol = color("#CEE4AE");  // natsumushi
-    break;
-  case 8 : 
-    pnlCol = color("#BBBCDA");  // huji
-    break;
-  case 16 : 
-    pnlCol = color("#D6C6AF");  // ama
-    break; 
-  case 32 : 
-    pnlCol = color("#D9A62E");  // hajizome
-    break;
-  case 64 : 
-    pnlCol = color("#68BE8D");  // wakatake
-    break;  
-  case 128 : 
-    pnlCol = color("#028760");  // tokiwa midori
-    break; 
-  case 256 : 
-    pnlCol = color("#5654A2");  // kikyo
-    break; 
-  case 512 : 
-    pnlCol = color("#0095D9");  // ao
-    break;
-  case 1024 : 
-    pnlCol = color("#F39800");  // kintya
-    break; 
-  case 2048 : 
-    pnlCol = color("#E2041B");  // shoujyouhi
-    break;
-  case 4096 :
-    pnlCol = color("#AA4C8F");  // ume murasaki
-    break;
-  case 8192 :
-    pnlCol = color("#9F6F55");  // tonotya
-    break;
-  default : 
-    pnlCol = color("#EEBBCB");  // nadesiko
-    break;
+  const colorDict = [
+    "#D3D3D3", "#A0D8EF", "#CEE4AE", "#BBBCDA", "#D6C6AF", "#D9A62E", 
+    "#68BE8D", "#028760", "#5654A2", "#0095D9", "#F39800", "#E2041B", 
+    "#AA4C8F", "#9F6F55"
+  ];
+
+  let index = this.binlog(panelNum);
+
+  return ( index < colorDict.length )? colorDict[index] : "#EEBBCB";
+};
+
+
+View.prototype.binlog = function(n) {
+
+  if ( n == 0 ) {
+    return 0;
   }
 
-  return pnlCol;
+  let logn;
+  for ( logn = 0; n > 0; n = (int)(n/2), logn++ );
+
+  return (int)(logn-1);
 };
 
 
@@ -136,15 +108,17 @@ View.prototype.initNewPanelSize = function() {
 
 View.prototype.finMessage = function() {
 
-  /* draw completely */
+  /* draw panels completely */
   this.newPanelSize = PANELSIZE;
   this.board();
 
+  /* fin-message panel */
   strokeWeight(5);
   stroke("#E6E6FA");  // lavender
   fill(255, 255, 255, 220);
   rect(0, 0, BOARDSIZE, BOARDSIZE);
 
+  /* message */
   noStroke();
   fill("#000000");  // black
   textSize(16);
